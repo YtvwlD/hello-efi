2a38d06e04821a35f6453a8d1ae5538df6e71590
Niklas Sombert <niklas@ytvwld.de>
2023-04-28 18:52:08 +0200

    hello world

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,20 @@
 #![no_std]
 
 use uefi::prelude::*;
+use uefi_services::println;
 
 #[entry]
 fn main(_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     uefi_services::init(&mut system_table).unwrap();
-
-    Status::SUCCESS
+    println!("Hello, world!");
+    println!(
+        "(running on {} {} which claims to be compatible to UEFI {})",
+        system_table.firmware_vendor(),
+        system_table.firmware_revision(),
+        system_table.uefi_revision(),
+    );
+    println!("looping...");
+    loop {
+        core::hint::spin_loop()
+    }
 }

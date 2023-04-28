eeb5f76ceb37fd0e769eb608671b3337b29d3105
Niklas Sombert <niklas@ytvwld.de>
2023-04-28 18:48:14 +0200

    uefi-rs template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+#![no_main]
+#![no_std]
+
+use uefi::prelude::*;
+
+#[entry]
+fn main(_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
+    uefi_services::init(&mut system_table).unwrap();
+
+    Status::SUCCESS
 }
